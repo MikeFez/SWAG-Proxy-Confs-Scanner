a814e8233a3f58929f9058885ea446e49186a349
Michael Fessenden <MikeFez@gmail.com>
2023-03-10 13:24:06 -0500

    Fix scanning

--- swag_proxy_confs_scanner/app.py
+++ swag_proxy_confs_scanner/app.py
@@ -10,13 +10,13 @@ def get_subdomain_confs():
     return glob.glob(path)
 
 
-def get_file_contents(conf_file_path):
+def get_conf_contents(conf_file_path):
     with open(conf_file_path, "r") as f:
         contents = f.readlines()
     return contents
 
 
-def parse_conf_configuration(file_contents):
+def parse_conf_configuration(conf_contents):
     data = {"locations": {}}
 
     # Server Portion
@@ -31,8 +31,8 @@ def parse_conf_configuration(file_contents):
     UPSTREAM_PORT_EXPR = r'^\s*set\s\$upstream_port\s(\S+);\s*$'
     UPSTREAM_PROTO_EXPR = r'^\s*set\s\$upstream_proto\s(\S+);\s*$'
 
-    server_portion = re.sub(LOCATION_SEC_EXPR, "", file_contents)
-    location_portions = {loc_name: loc_contents for loc_name, loc_contents in re.findall(LOCATION_SEC_EXPR, file_contents, re.MULTILINE)}
+    server_portion = re.sub(LOCATION_SEC_EXPR, "", conf_contents)
+    location_portions = {loc_name: loc_contents for loc_name, loc_contents in re.findall(LOCATION_SEC_EXPR, conf_contents, re.MULTILINE)}
 
     # Server Portion
     subdomain = re.search(SERVER_NAME_EXPR, server_portion, re.MULTILINE)
@@ -46,19 +46,20 @@ def parse_conf_configuration(file_contents):
 
     # Location Section
     for location_name, location_config in location_portions.items():
-        data["locations"][location_name] = {}
+        location_data = {}
 
         authelia_loc_enabled = re.search(AUTHELIA_LOCATION_ENABLED_EXPR, location_config, re.MULTILINE)
-        data["locations"][location_name]['authelia'] = True if authelia_loc_enabled is not None else False
+        location_data['authelia'] = True if authelia_loc_enabled is not None else False
 
         upstream_app = re.search(UPSTREAM_APP_EXPR, location_config, re.MULTILINE)
-        data["locations"][location_name]['upstream_app'] = upstream_app[1] if upstream_app is not None else None
+        location_data['upstream_app'] = upstream_app[1] if upstream_app is not None else None
 
         upstream_port = re.search(UPSTREAM_PORT_EXPR, location_config, re.MULTILINE)
-        data["locations"][location_name]['upstream_port'] = upstream_port[1] if upstream_port is not None else None
+        location_data['upstream_port'] = upstream_port[1] if upstream_port is not None else None
 
         upstream_proto = re.search(UPSTREAM_PROTO_EXPR, location_config, re.MULTILINE)
-        data["locations"][location_name]['upstream_proto'] = upstream_proto[1] if upstream_proto is not None else None
+        location_data['upstream_proto'] = upstream_proto[1] if upstream_proto is not None else None
+        data["locations"][location_name] = location_data
     return data
 
 
@@ -67,5 +68,6 @@ if __name__ == "__main__":
     global_data = {}
     conf_locations = get_subdomain_confs()
     for conf_location in conf_locations:
-        global_data[conf_location] = parse_conf_configuration(conf_location)
+        conf_contents = get_conf_contents(conf_location)
+        global_data[conf_location] = parse_conf_configuration(conf_contents)
     print(global_data)
